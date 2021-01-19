571ce7f08c41994bbe56aacb32aecd999d9dd3c5
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2021-01-19 11:49:48 +0200

    hero section complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+const body = document.querySelector('body')
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', () => {
+    if (header.classList.contains('open')) {
+        fadeElems.forEach((el) => {
+            el.classList.remove('fade-in');
+            el.classList.add('fade-out');
+        });
+        header.classList.remove('open');
+    } else {
+        header.classList.add('open');
+        
+        fadeElems.forEach((el) => {
+            el.classList.remove('fade-out');
+            el.classList.add('fade-in');
+        });
+        
+    }
+
+});
